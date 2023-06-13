b7f18632d7c815f5a628a1b01a79efdf0ee2a343
willowsenator <willowsenator@gmail.com>
2023-06-13 23:34:59 +0200

    First configuration for hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers :[
+      {version: "0.5.5"},
+      {version: "0.6.6"},
+      {version: "0.8.8"},
+    ]
+  },
+  networks:{
+    hardhat:{
+      forking:{
+        url: "https://bsc-dataseed.binance.org",
+      }
+    },
+    testnet:{
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts :[""] // Private key to deploy
+    },
+    mainnet:{
+      url: "https://bsc-dataseed.binance.org",
+      chainId: 56,
+    }
+  }
+};
